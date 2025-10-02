834e92662bfda85b7522aaba46099018022f6322
Manea Diana-Valentina <maneadiana-valentina@Maneas-MacBook-Pro.local>
2025-10-02 09:43:21 +0300

    refactor: Cleanup and improve readability of OrderService

--- src/main/java/com/dvm/store_management_tool/product_service/service/OrderService.java
+++ src/main/java/com/dvm/store_management_tool/product_service/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.dvm.store_management_tool.product_service.service;
 
 import com.dvm.store_management_tool.product_service.dto.order.CreateOrderRequest;
+import com.dvm.store_management_tool.product_service.dto.order_item.CreateOrderItemRequest;
 import com.dvm.store_management_tool.product_service.entity.Order;
 import com.dvm.store_management_tool.product_service.entity.OrderItem;
 import com.dvm.store_management_tool.product_service.entity.Product;
@@ -9,12 +10,13 @@ import com.dvm.store_management_tool.product_service.exception.NotEnoughProducts
 import com.dvm.store_management_tool.product_service.exception.OrderNotFoundException;
 import com.dvm.store_management_tool.product_service.exception.ProductNotFoundException;
 import com.dvm.store_management_tool.product_service.exception.UserNotFoundException;
-import com.dvm.store_management_tool.product_service.mapper.OrderItemDtoMapper;
 import com.dvm.store_management_tool.product_service.repository.OrderJpaRepository;
 import com.dvm.store_management_tool.product_service.repository.ProductJpaRepository;
 import com.dvm.store_management_tool.product_service.repository.UserJpaRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -29,45 +31,30 @@ public class OrderService {
     private final ProductJpaRepository productJpaRepository;
     private final UserJpaRepository userJpaRepository;
 
+    @Transactional
     public Order createNewOrder(CreateOrderRequest request) {
-        Order order = new Order();
-        User createdBy = userJpaRepository.findById(request.createdById()).orElseThrow(() -> {
-            log.warn("User not found with id {}", request.createdById());
-            return new UserNotFoundException(request.createdById());
-        });
+        final User createdBy = getCurrentlyAuthenticatedUser();
+        log.info("The user creating the order: {}", createdBy);
+
+        final Order order = new Order();
         order.setCreatedBy(createdBy);
+        order.setTotalAmount(BigDecimal.ZERO);
 
-        log.info("User that created the order: {}", createdBy);
+        List<OrderItem> orderItems = mapCreateOrderRequestToOrderItems(request);
 
-        List<OrderItem> orderItems = request.orderItems().stream()
-                        .map(item ->
-                                OrderItemDtoMapper.mapCreateRequestDtoToOrderItem(item, productJpaRepository.getReferenceById(item.productId())))
-                .toList();
+        orderItems.forEach(this::updateAndValidateStock);
 
-        for(OrderItem orderItem : orderItems) {
-            Product product = productJpaRepository.findById(orderItem.getProduct().getId())
-                    .orElseThrow(() -> new ProductNotFoundException(orderItem.getProduct().getId()));
+        orderItems.forEach(order::addOrderItem);
 
-            OrderItem item = OrderItem.builder()
-                    .product(product)
-                    .totalPrice(BigDecimal.valueOf(orderItem.getQuantity()).multiply(product.getPrice()))
-                    .quantity(orderItem.getQuantity())
-                    .build();
+        order.setTotalAmount(orderItems.stream()
+                .map(OrderItem::getTotalPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add));
 
-            int stockLeft = product.getStock() - orderItem.getQuantity();
-            if(stockLeft <= 0) {
-                log.warn("Product {} has no stock left", orderItem.getProduct().getId());
-                throw new NotEnoughProductsException(product.getName(), product.getStock(), orderItem.getQuantity());
-            }
-            else {
-                product.setStock(stockLeft);
-                productJpaRepository.save(product);
-                order.addOrderItem(item);
-                order.setTotalAmount(order.getTotalAmount().add(item.getTotalPrice()));
-            }
-        }
+        Order savedOrder = orderJpaRepository.save(order);
+
         log.info("Order has been successfully created: {}", order);
-        return orderJpaRepository.save(order);
+
+        return savedOrder;
     }
 
     public List<Order> findAllOrders() {
@@ -78,5 +65,43 @@ public class OrderService {
     public Order findOrderById(final Long id) {
         return orderJpaRepository.findById(id)
                 .orElseThrow(() -> new OrderNotFoundException(id));
+    }
+
+    private User getCurrentlyAuthenticatedUser() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        return userJpaRepository.findByUsername(username).orElseThrow(() -> {
+            log.warn("User not found with username {}", username);
+            return new UserNotFoundException(username);
+        });
+    }
+
+    private List<OrderItem> mapCreateOrderRequestToOrderItems(CreateOrderRequest createOrderRequest) {
+        return createOrderRequest.orderItems().stream()
+                .map((CreateOrderItemRequest item) ->
+                        {
+                            final Product product = productJpaRepository.findById(item.productId())
+                                    .orElseThrow(() -> new ProductNotFoundException(item.productId()));
+
+                            return OrderItem.builder()
+                                    .product(product)
+                                    .totalPrice(BigDecimal.valueOf(item.quantity()).multiply(product.getPrice()))
+                                    .quantity(item.quantity())
+                                    .build();
+                        })
+                .toList();
+    }
+
+    private void updateAndValidateStock (OrderItem orderItem) {
+        Product product = orderItem.getProduct();
+        int stockLeft = product.getStock() - orderItem.getQuantity();
+
+        if(stockLeft < 0) {
+            log.warn("Not enough products of type {} available. Requested: {}. Available: {}", product.getName(), orderItem.getQuantity(), product.getStock());
+            throw new NotEnoughProductsException(product.getName(), product.getStock(), orderItem.getQuantity());
+        }
+        product.setStock(stockLeft);
+        productJpaRepository.save(product);
+        log.info("Stock of product {} updated. Remaining products: {}", product.getName(), product.getStock());
     }
 }
